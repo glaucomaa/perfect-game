0bd58064503a684557dbb401ef2f5b25ecce2af3
glaucomaa <glaucomaa>
2021-11-14 22:27:51 +0300

    add docs in "db.cpp"

--- PerfectGame/DataBaseConnector/db.cpp
+++ PerfectGame/DataBaseConnector/db.cpp
@@ -4,7 +4,22 @@
 #include "Network.h"
 #include "../GameState/Player.h"
 
+/*
 
+Реализовал вам две функции, одна для записи в бд, другая для чтения из бд:
+send_user_to_db(Player& plr) и get_user_from_db(std::string&& name, Json::Value& body) 
+соответственно.
+В первую передаётся объект игрока по ссылке, возвращается код,
+если он равен 0 - то успешно,
+иначе - произошла какая-то ошибка.
+Во вторую передаётся имя игрока(id) и объект типа Json::Value, который создаётся снаружи,
+если код 0 - успешно,
+если код 1 - то такого игрока нет,
+иначе - ошибка.
+Объект типа Json::Value нужен для передачи в метод GameState.addPlayer(Json::Value& obj, sock)
+этот метод был написан аналогично вашему addPlayer(...).
+
+*/
 static WSASession Session;
 static UDPSocket Socket;
 static const unsigned short buf_size = 1024;
